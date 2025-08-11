f749abcf6e7f684511bf99080b19c4a6c2cb1d87
Priya@0499 <Ppworkspac>
2025-08-11 23:07:45 +0530

    nothing

--- main/ota.h
+++ main/ota.h
@@ -5,6 +5,8 @@
 
 #define APP_VERSION "1.0.1"  // Current firmware version string
 
+//#define APP_VERSION "1.0.2"  // Current firmware version string
+
 // Call this function to check and perform OTA update if new firmware is available
 esp_err_t ota_check_and_update(void);
 
